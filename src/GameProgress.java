5ea63f6925349312a400bdbbb0ff4c1e1ce655e9
Sudarev <soul7341@gmail.com>
2023-10-28 00:08:13 +0300

    Created the saving game

---
+++ src/GameProgress.java
@@ -0,0 +1,36 @@
+import java.io.*;
+
+public class GameProgress implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private int health;
+    private int weapons;
+    private int lvl;
+    private double distance;
+
+    public GameProgress(int health, int weapons, int lvl, double distance) {
+        this.health = health;
+        this.weapons = weapons;
+        this.lvl = lvl;
+        this.distance = distance;
+    }
+
+    public void saveGame(String path) {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(path, true))) {
+            oos.writeObject(this);
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+
+
+    @Override
+    public String toString() {
+        return "GameProgress{" +
+                "health=" + health +
+                ", weapons=" + weapons +
+                ", lvl=" + lvl +
+                ", distance=" + distance +
+                '}';
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,39 @@
+import java.io.*;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
 public class Main {
     public static void main(String[] args) {
-        String path = "/Users/Sydarrr/Desktop/testHW/Games";
-        CreateFile createFile = new CreateFile(path);
-        createFile.createFolder();
-        createFile.setPath(path + "/src");
-        createFile.createFolder();
-        createFile.setPath(path + "/res");
-        createFile.createFolder();
-        createFile.setPath(path + "/res/drawables");
-        createFile.createFolder();
-        createFile.setPath(path + "/res/vectors");
-        createFile.createFolder();
-        createFile.setPath(path + "/res/icons");
-        createFile.createFolder();
-        createFile.setPath(path + "/savegames");
-        createFile.createFolder();
-        createFile.setPath(path + "/src/test");
-        createFile.createFolder();
-        createFile.setPath(path + "/src/main");
-        createFile.createFolder();
-        createFile.createFile("Main.java");
-        createFile.createFile("Utils.java");
-        createFile.setPath(path + "/temp");
-        createFile.createFolder();
-        createFile.createFile("temp.txt");
-        createFile.end();
+        //это распаковка игры (1 задание)
+        var gu = new GameUnpacker();
+        gu.unpack();
+        //сохранение прогресса (задание 2)
+        GameProgress gp1 = new GameProgress(100, 3, 80, 15.5);
+        GameProgress gp2 = new GameProgress(50, 2, 15, 10.3);
+        GameProgress gp3 = new GameProgress(30, 5, 120, 43.1);
+        String path = "/Users/Sydarrr/Desktop/testHW/Games/savegames/saves.txt";
+        File file = new File(path);
+        try {
+            file.createNewFile();
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+
+        gp1.saveGame(path);
+
+        try (ZipOutputStream zout = new ZipOutputStream(new FileOutputStream("/Users/Sydarrr/Desktop/testHW/Games/savegames/saves.zip"))){
+            ZipEntry ze = new ZipEntry("saves.txt");
+            zout.putNextEntry(ze);
+            FileInputStream fis = new FileInputStream("/Users/Sydarrr/Desktop/testHW/Games/savegames/saves.txt");
+            byte[] buffer = new byte[fis.available()];
+            fis.read(buffer);
+            zout.write(buffer);
+            zout.closeEntry();
+            file.delete();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
